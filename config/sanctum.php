6714849db09ce0bac6bf265d3a0625b34c5f131a
hessam72 <hessamtahani@gmail.com>
2024-02-01 18:20:20 +0330

    last commit

--- config/sanctum.php
+++ config/sanctum.php
@@ -15,10 +15,11 @@ return [
     |
     */
 
-    'stateful' => explode(',', env(
-        'SANCTUM_STATEFUL_DOMAINS',
-        'localhost,localhost:8080,127.0.0.1,127.0.0.1:8080,::1'
-    )),
+    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
+        '%s%s',
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
+        Sanctum::currentApplicationUrlWithPort()
+    ))),
 
     /*
     |--------------------------------------------------------------------------
