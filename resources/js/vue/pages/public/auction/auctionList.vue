027a6a45ae12fa83aed1ceabddf7e586d4d00e3f
hessam72 <hessamtahani@gmail.com>
2024-02-26 13:52:32 +0330

    auction list starting

---
+++ resources/js/vue/pages/public/auction/auctionList.vue
@@ -0,0 +1,14 @@
+<template>
+  <hero-section></hero-section>
+  list
+</template>
+<script>
+import heroSection from "../../../components/auctions/hero.vue";
+export default {
+  components: {
+    heroSection,
+  },
+};
+</script>
+<style lang="scss" scoped>
+</style>
--- resources/js/vue/router/router.js
+++ resources/js/vue/router/router.js
@@ -2,7 +2,8 @@ import { createRouter, createWebHistory } from "vue-router";
 import store from "../store/index.js";
 
 import homePage from "../pages/public/homePage/index.vue"
-import AuctionList from "../pages/public/auction/all.vue"
+import AuctionList from "../pages/public/auction/auctionList.vue"
+import AuctionList_old from "../pages/public/auction/all.vue"
 import AuctionIndex from "../pages/public/auction/index.vue"
 import UserIndex from "../pages/public/guarded/user/index.vue"
 
