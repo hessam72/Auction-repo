c788e1f914d39edf858845500f684827fc47d4c6
hessam72 <hessamtahani@gmail.com>
2024-03-01 19:42:53 +0330

    ,

---
+++ resources/js/vue/components/auctions/index/product_content.vue
@@ -0,0 +1,47 @@
+<template>
+     <div class="product-info-container flex flex-col">
+                <div class="product-header flex items-center justify-between">
+                    <h2>Product Name</h2>
+                    <h2>$1750</h2>
+                </div>
+                <div class="product-content">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis, obcaecati omnis culpa recusandae
+                        odit, placeat labore dolor beatae sed incidunt quidem delectus nemo voluptatum consectetur facilis
+                        molestias nobis voluptas saepe.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis illo et exercitationem? Quod
+                        eaque, earum voluptatem vero odio animi assumenda. Optio quos aut necessitatibus maiores illum error
+                        ipsum nisi quo.;Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus alias dolore neque
+                        ullam, maxime molestiae est ex suscipit aperiam debitis inventore rem doloremque tempore tenetur
+                        distinctio sunt, officia, dolorem voluptatibus?
+                    </p>
+                </div>
+            </div>
+</template>
+<script>
+</script>
+<style scoped lang="scss">
+
+
+.product-info-container{
+    padding-top: 3rem;
+    border-top: 1.5px solid #777;
+    width: 95%;
+    margin: 3rem auto;
+    background-color: #ffffffab;
+    padding: 2rem;
+    border-radius: 15px;
+    box-shadow: 0 3px 10px #aaa;
+    .product-header{
+        font-size: 1.6rem;
+    font-weight: 600;
+    margin-bottom: 2rem;
+    }
+    .product-content{
+p{
+    font-size: 1.25rem;
+    line-height: 2.2rem;
+    text-align: justify;
+}
+    }
+}
+</style>
---
+++ resources/js/vue/components/auctions/index/reviews.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="reviews-container">
+        <div class="header flex justify-between items-center">
+            <h2>Reviews (35)</h2>
+            <div class="total-score"></div>
+        </div>
+        <div class="review-container">
+            <div class="user-info flex flex-col justify-center items-center"></div>
+            <div class="content-container flex flex-col">
+                <div class="content"></div>
+                <div class="ratings"></div>
+            </div>
+        </div>
+    </div>
+</template>
+<style scoped lang="scss">
+</style>
