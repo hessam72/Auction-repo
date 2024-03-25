b094ef8a45459fc10b72089f1638e97a7904e15b
hessam72 <hessamtahani@gmail.com>
2024-03-25 21:08:38 +0330

    ,,

---
+++ database/migrations/2024_03_25_172719_create_visitors_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    // TODO - add https://github.com/shetabit/visitor for tracking user
+    public function up(): void
+    {
+        Schema::create('visitors', function (Blueprint $table) {
+            $table->id();
+            $table->string('ip_address');
+            $table->string('browser')->nullable();
+            $table->string('device')->nullable();
+
+            $table->string('user_agent');
+
+            $table->timestamp('visited_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('visitors');
+    }
+};
